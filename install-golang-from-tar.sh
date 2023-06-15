adf8bbb0096b18404cfee8a4ab44383683b426ba
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-14 17:07:33 -0700

    Don't duplicate path for golang

--- install-golang-from-tar.sh
+++ install-golang-from-tar.sh
@@ -5,9 +5,12 @@ version=$(curl https://go.dev/VERSION?m=text | sed -e 's#go##')
 echo $version
 curl -Lo /usr/local/src/go${version}.linux-amd64.tar.gz https://go.dev/dl/go${version}.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf /usr/local/src/go${version}.linux-amd64.tar.gz
+if ! grep --silent 'PATH=$PATH:/usr/local/go/bin' ~/.bashrc; then
+    echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.bashrc
+fi
 export PATH=$PATH:/usr/local/go/bin
-echo 'export PATH=$PATH:/usr/local/go/bin' >>~/.bashrc
 go version
 EOF
+
 export PATH=$PATH:/usr/local/go/bin
 bash -u -e -x /tmp/goinstall.sh
