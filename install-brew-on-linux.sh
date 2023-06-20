b0d4642c02ae3421836ae5a9a34f2e0db818e885
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-20 11:00:13 -0400

    Fiddle

--- install-brew-on-linux.sh
+++ install-brew-on-linux.sh
@@ -2,12 +2,13 @@
 
 if command -v apt &>/dev/null; then
 #    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "skipping for now since you're running as root"
+    echo skipping for now since you are running as root
 
 elif command -v yum &>/dev/null; then
     sudo yum -y groupinstall 'Development Tools'
     brew install gcc
 
 else
-    echo "Neither apt nor yum package manager is available."
+    echo neither apt nor yum package manager is available
+
 fi
