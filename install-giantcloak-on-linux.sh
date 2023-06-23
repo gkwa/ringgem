1f76cdd2dd21993fe1bf57378edab7214edba8fe
Taylor Monacelli <taylormonacelli@gmail.com>
2023-06-23 16:09:54 -0400

    Giantcloak

--- install-giantcloak-on-linux.sh
+++ install-giantcloak-on-linux.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 curl -Lo /tmp/giantcloak_Linux_x86_64.tar.gz https://github.com/taylormonacelli/giantcloak/releases/latest/download/giantcloak_Linux_x86_64.tar.gz
+rm -f /tmp/giantcloak
 tar xzf /tmp/giantcloak_Linux_x86_64.tar.gz -C /tmp
 install --mode 0755 --group root --owner root /tmp/giantcloak /usr/local/bin/giantcloak
