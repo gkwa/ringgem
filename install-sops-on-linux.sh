9bfdfa15033026579b875e0dba5d4914a77c5074
Taylor Monacelli <taylormonacelli@gmail.com>
2023-07-15 11:43:21 -0700

    grep -Po... is doesn't work on all distros

--- install-sops-on-linux.sh
+++ install-sops-on-linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-version=$(curl --silent https://api.github.com/repos/getsops/sops/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+version=$(curl --silent https://api.github.com/repos/getsops/sops/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2 | tr -d v)
 echo $version
-curl -sSL -o /usr/local/bin/sops https://github.com/mozilla/sops/releases/download/$version/sops-${version}.linux
+curl -sSL -o /usr/local/bin/sops https://github.com/mozilla/sops/releases/download/v$version/sops-v${version}.linux
 chmod +x /usr/local/bin/sops
 sops --version
