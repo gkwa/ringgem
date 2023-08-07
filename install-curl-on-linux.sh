e3f42c47a3df9fe263b55472889e6198287936db
Taylor Monacelli <taylormonacelli@gmail.com>
2023-08-07 16:19:28 -0700

    Add curl

---
+++ install-curl-on-linux.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if command -v apt &>/dev/null; then
+    apt-get --yes install curl
+elif command -v yum &>/dev/null; then
+    yum -y install curl
+else
+    echo "Neither apt nor yum package manager is available."
+fi
--- install-sops-on-linux.sh
+++ install-sops-on-linux.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl --silent https://api.github.com/repos/getsops/sops/releases/latest | grep tag_name | cut -d: -f2 | cut -d\" -f2 | tr -d v)
-echo $version
+version=$(curl -sSL https://api.github.com/repos/getsops/sops/releases/latest | grep tag_name | cut -d: -f2- | tr -d ' ",v')
 curl -sSL -o /usr/local/bin/sops https://github.com/mozilla/sops/releases/download/v$version/sops-v${version}.linux
 chmod +x /usr/local/bin/sops
 sops --version
