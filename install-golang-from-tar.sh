be44f912148ef657c0380039595176ee4b7584b3
Taylor Monacelli <taylormonacelli@gmail.com>
2023-08-08 18:52:48 -0700

    Fix go install

--- install-golang-from-tar.sh
+++ install-golang-from-tar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cat >/tmp/goinstall.sh <<'EOF'
-version=$(curl https://go.dev/VERSION?m=text | sed -e 's#go##')
+version=$(curl https://go.dev/VERSION?m=text | grep -v time | sed -e 's#go##')
 echo $version
 curl -Lo /usr/local/src/go${version}.linux-amd64.tar.gz https://go.dev/dl/go${version}.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf /usr/local/src/go${version}.linux-amd64.tar.gz
