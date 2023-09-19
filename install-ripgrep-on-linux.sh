6e56794a98a289e4641fc70137859211478689f2
Taylor Monacelli <taylormonacelli@gmail.com>
2023-09-19 11:15:13 -0700

    Improve ripgrep install

--- install-ripgrep-on-linux.sh
+++ install-ripgrep-on-linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-version=$(curl -s https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
-curl -Lo /usr/local/src/ripgrep-${version}-x86_64-unknown-linux-musl.tar.gz https://github.com/BurntSushi/ripgrep/releases/download/$version/ripgrep-${version}-x86_64-unknown-linux-musl.tar.gz
-tar xzf /usr/local/src/ripgrep-${version}-x86_64-unknown-linux-musl.tar.gz -C /tmp
-install --mode 755 /tmp/ripgrep-${version}-x86_64-unknown-linux-musl/rg /usr/local/bin/rg
+# install ripgrep
+version=$(wget -qO - https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | grep tag_name | cut -d: -f2- | tr -d ' \":,')
+base=ripgrep-${version}-x86_64-unknown-linux-musl
+wget -qO - https://github.com/BurntSushi/ripgrep/releases/download/$version/$base.tar.gz | tar -C /usr/local/bin --no-same-owner -xz $base/rg
 rg --version
