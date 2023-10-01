1421cc3dbbcbfc3d1211ab3e37dee621b4858b71
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-01 16:30:17 -0700

    Add terragrunt

---
+++ install-terragrunt-on-linux.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -L -o /usr/local/bin/terragrunt https://github.com/gruntwork-io/terragrunt/releases/latest/download/terragrunt_linux_amd64
+chmod +x /usr/local/bin/terragrunt
