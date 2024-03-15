141db3aaf803edc64c9d4ca94164b7950f37b1b6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-15 09:10:22 -0700

    don't allow gpg to prompt to overwrite, just doit

--- install-logstash-on-ubuntu.sh
+++ install-logstash-on-ubuntu.sh
@@ -3,13 +3,13 @@
 # https://www.elastic.co/guide/en/logstash/8.12/installing-logstash.html#_apt
 
 export NEEDRESTART_MODE=a
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
 apt-get --assume-yes install apt-transport-https
 echo "deb [signed-by=/usr/share/keyrings/elastic-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
 
 apt-get update
 
 apt-get --assume-yes install logstash
-ln -s /usr/share/logstash/bin/logstash /usr/local/bin/logstash
+ln --force --symbolic /usr/share/logstash/bin/logstash /usr/local/bin/logstash
 
 logstash --version
