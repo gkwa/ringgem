03124e37d5dec73672a7b24e236f53bb8d77c41f
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-15 09:22:49 -0700

    Fix dpkg-preconfigure: unable to re-open stdin: No such file or directory

--- install-logstash-on-ubuntu.sh
+++ install-logstash-on-ubuntu.sh
@@ -2,7 +2,10 @@
 
 # https://www.elastic.co/guide/en/logstash/8.12/installing-logstash.html#_apt
 
+export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
+
+
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
 apt-get --assume-yes install apt-transport-https
 echo "deb [signed-by=/usr/share/keyrings/elastic-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
