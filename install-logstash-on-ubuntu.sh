6311b6ebed63665aa6c522b9c1ac5b6c78d36b9b
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-15 09:52:15 -0700

    workaround failing install

--- install-logstash-on-ubuntu.sh
+++ install-logstash-on-ubuntu.sh
@@ -2,10 +2,11 @@
 
 # https://www.elastic.co/guide/en/logstash/8.12/installing-logstash.html#_apt
 
+
 export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
 
-
+set +e
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
 apt-get --assume-yes install apt-transport-https
 echo "deb [signed-by=/usr/share/keyrings/elastic-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
