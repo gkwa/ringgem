4e1fcb8f7aa5059def3018a00c4536410d22cdc6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-16 18:02:53 -0700

    Add multiline plugin for logstash

--- install-logstash-on-ubuntu.sh
+++ install-logstash-on-ubuntu.sh
@@ -17,3 +17,9 @@ apt-get --assume-yes install logstash
 ln --force --symbolic /usr/share/logstash/bin/logstash /usr/local/bin/logstash
 
 logstash --version
+
+
+## Add logstash plugin
+cd /usr/share/logstash
+bin/logstash-plugin install logstash-filter-multiline
+bin/logstash-plugin list | grep multiline
