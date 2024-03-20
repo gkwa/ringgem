b63942b787a87fcea44a6b195474860886f255b0
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-20 11:55:56 -0700

    Cleanup cloudwatch deb

--- install-cloudwatch-on-ubuntu.sh
+++ install-cloudwatch-on-ubuntu.sh
@@ -1,3 +1,4 @@
 url=https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
 curl -o /tmp/amazon-cloudwatch-agent.deb $url
 dpkg --install --skip-same-version /tmp/amazon-cloudwatch-agent.deb
+rm -f /tmp/amazon-cloudwatch-agent.deb
