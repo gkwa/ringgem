5819b70706e951955a07068a0769edc36e936a27
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-19 17:01:52 -0700

    overwrite gpg

--- install-terraform-on-ubuntu.sh
+++ install-terraform-on-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -L https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+curl -L https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get --assume-yes install terraform
