05347160d3ad2c4a20874cf378b8ba06a20ff4c8
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-19 17:15:31 -0700

    fix

--- install-ripgrep-on-linux.sh
+++ install-ripgrep-on-linux.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# install ripgrep
-version=$(wget -qO - https://api.github.com/repos/BurntSushi/ripgrep/releases/latest | grep tag_name | cut -d: -f2- | tr -d ' \":,')
-base=ripgrep-${version}-x86_64-unknown-linux-musl
-wget -qO - https://github.com/BurntSushi/ripgrep/releases/download/$version/$base.tar.gz | tar -C /usr/local/bin --no-same-owner -xz $base/rg
+release_url=https://api.github.com/repos/BurntSushi/ripgrep/releases/latest
+version=$(curl -fsSL $release_url | grep tag_name | cut -d: -f2- | tr -d ' \":,')
+base=ripgrep-$version-x86_64-unknown-linux-musl
+url=https://github.com/BurntSushi/ripgrep/releases/download/$version/$base.tar.gz
+curl -fsSL $url | tar -C /usr/local/bin --no-same-owner -xz $base/rg
+
+ln -fs /usr/local/bin/$base/rg /usr/local/bin/rg
 rg --version
