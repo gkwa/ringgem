d704cd949fbae1a74a4f1f67547cb6446a3b1a89
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-20 09:46:16 -0700

    wget: don't show progressbar please

--- install-sops-on-linux.sh
+++ install-sops-on-linux.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+set -u
+
 version=$(curl -fsSL https://api.github.com/repos/getsops/sops/releases/latest | grep tag_name | cut -d: -f2- | tr -d ' ,v\"')
 url=https://github.com/mozilla/sops/releases/download/v$version/sops-v$version.linux.amd64
 rm -f /tmp/sops
-wget --output-document=/tmp/sops $url
+wget --quiet --output-document=/tmp/sops $url
 install --mode 0755 --group root --owner root /tmp/sops /usr/local/bin/sops
 rm /tmp/sops
 sops --version
