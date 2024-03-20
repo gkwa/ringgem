fea5a817c27f7c9ebcf4062d47160cc733ab6376
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-20 09:20:08 -0700

    Add updatedb

---
+++ install-updatedb-on-linux.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if command -v apt-get >/dev/null; then
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get install --assume-yes mlocate
+elif command -v yum >/dev/null; then
+    yum install --assume-yes mlocate
+elif command -v apk >/dev/null; then
+    apk add --no-cache mlocate
+elif command -v dnf >/dev/null; then
+    dnf install --assume-yes mlocate
+else
+    echo "Unsupported package manager, couldn't install updatedb"
+    exit 1
+fi
