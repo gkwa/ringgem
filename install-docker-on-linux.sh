1200990b845c8af13cc302f86761ec5212d99130
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-19 23:25:42 -0700

    debs should be installed serially

--- install-docker-on-linux.sh
+++ install-docker-on-linux.sh
@@ -2,8 +2,10 @@
 
 if command -v apt &>/dev/null; then
     curl -fsSL https://get.docker.com/ | sh
+
 elif command -v yum &>/dev/null; then
     yum -y install docker
+
 else
     echo "Neither apt nor yum package manager is available."
 fi
--- install-git-on-linux.sh
+++ install-git-on-linux.sh
@@ -2,8 +2,10 @@
 
 if command -v apt &>/dev/null; then
     DEBIAN_FRONTEND=noninteractive apt-get -y install git
+
 elif command -v yum &>/dev/null; then
     yum -y install git
+
 else
     echo "Neither apt nor yum package manager is available."
 fi
