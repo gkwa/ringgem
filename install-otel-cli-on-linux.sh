a381014e6ea002cd59878da349caac3844479e37
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-21 17:17:23 -0700

    cleanup tar

--- install-otel-cli-on-linux.sh
+++ install-otel-cli-on-linux.sh
@@ -4,10 +4,11 @@
 # https://github.com/equinix-labs/otel-cli/releases
 
 version=$(curl -s https://api.github.com/repos/equinix-labs/otel-cli/releases/latest | grep tag_name | cut -d '"' -f 4)
-tar=otel-cli_$(echo $version | cut -d 'v' -f 2)_linux_amd64.tar.gz
-curl -Lo /tmp/$tar https://github.com/equinix-labs/otel-cli/releases/download/$version/$tar
-tar -xzf $tar
-install --mode 0755 --group root --owner root /tmp/otel-cli /usr/local/bin/otel-cli
-rm -f /tmp/$tar
-rm -f /tmp/otel-cli
+base=otel-cli_$(echo $version | cut -d 'v' -f 2)_linux_amd64
+tar=$base.tar.gz
+mkdir -p /tmp/$base
+curl -Lo /tmp/$base/$tar https://github.com/equinix-labs/otel-cli/releases/download/$version/$tar
+tar -xzf /tmp/$base/$tar -C /tmp/$base
+install --mode 0755 --group root --owner root /tmp/$base/otel-cli /usr/local/bin/otel-cli
 otel-cli --help >/dev/null
+rm -rf /tmp/$base
