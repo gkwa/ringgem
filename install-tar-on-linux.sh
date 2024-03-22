12b2c2fef3faedf72f43e38c09d41d7384940c0c
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-21 17:10:36 -0700

    refactar tar

---
+++ install-tar-on-linux.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if command -v apt-get >/dev/null 2>&1; then
+    DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes tar
+elif command -v dnf >/dev/null 2>&1; then
+    sudo dnf install -y tar
+elif command -v yum >/dev/null 2>&1; then
+    sudo yum install -y tar
+elif command -v zypper >/dev/null 2>&1; then
+    sudo zypper install -y tar
+elif command -v apk >/dev/null 2>&1; then
+    sudo apk add tar
+else
+    echo "Unsupported package manager. Please install tar manually."
+    exit 1
+fi
