a8eb2abd77a04c9630a423de156ac79a2f221d59
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-24 06:44:51 -0700

    Add rye

---
+++ install-rye-on-linux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+RYE_HOME="$HOME/.rye"
+mkdir -p "$RYE_HOME"
+curl -sSf https://rye-up.com/get | RYE_INSTALL_OPTION="--yes" RYE_HOME="$RYE_HOME" bash
+
+if [[ ":$PATH:" != *":$RYE_HOME/shims:"* ]]; then
+    echo 'export PATH="$RYE_HOME/shims:$PATH"' >> "$HOME/.bashrc"
+    source "$HOME/.bashrc"
+fi
+
+source "$RYE_HOME/env"
+rye --version
