d0df1e7d0b5d88ec8c67925a8321c6b938b651c5
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-24 17:43:49 -0700

    whitespace

---
+++ install-boilerplate-on-linux-and-macos.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+version=$(curl -s https://api.github.com/repos/gruntwork-io/boilerplate/releases/latest | grep tag_name | cut -d: -f2 | tr -d 'v," ')
+
+kernel=$(uname)
+
+if [[ "$kernel" == Darwin ]]; then
+  binary=boilerplate_darwin_amd64
+elif [[ "$kernel" == Linux ]]; then
+  binary=boilerplate_linux_amd64
+else
+  echo Unsupported operating system: $kernel
+  exit 1
+fi
+
+echo $binary:$version
+
+curl -Lo /tmp/$binary https://github.com/gruntwork-io/boilerplate/releases/download/v$version/$binary
+
+install --mode 0755 --owner root --group wheel /tmp/$binary /usr/local/bin/boilerplate
+
+boilerplate --version
