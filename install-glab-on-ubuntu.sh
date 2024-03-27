fb05aa0f94f42fe1a7f86a292e168b0dd8b8e603
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-27 13:06:40 -0700

    Add wakemeops

--- install-glab-on-ubuntu.sh
+++ install-glab-on-ubuntu.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-curl -sSL https://raw.githubusercontent.com/upciti/wakemeops/main/assets/install_repository | bash
 apt-get --assume-yes install glab
 glab version
---
+++ install-wakemeops-on-ubuntu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -sSL https://raw.githubusercontent.com/upciti/wakemeops/main/assets/install_repository | bash
