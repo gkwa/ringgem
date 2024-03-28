38893ba7ae912a9b36033a2aac177623ccfb8334
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-28 10:39:38 -0700

    Workaround for E: Package 'default-jre' has no installation candidate

--- install-java-jre-on-ubuntu.sh
+++ install-java-jre-on-ubuntu.sh
@@ -2,8 +2,4 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-if apt-get --assume-yes install default-jre; then
-    exit 0
-fi
-
-apt-get --assume-yes install openjdk-11-jre
+apt-get --assume-yes install default-jre
