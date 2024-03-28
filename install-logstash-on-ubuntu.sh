1f4c478895ec28a0f04a2acde07c4b7146d6793f
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-28 12:06:04 -0700

    Fix Failed to fetch https://artifacts.elastic.co/packages/8.x/apt/pool/main/l/logstash/logstash-8.13.0-no-jdk.deb  404  Not Found

--- install-logstash-on-ubuntu.sh
+++ install-logstash-on-ubuntu.sh
@@ -1,10 +1,40 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+set -u
+
+
+export DEBIAN_FRONTEND=noninteractive
+export NEEDRESTART_MODE=a
+export NEEDRESTART_SUSPEND=1
+
+
+# workround
+if [[ ! -f logstash-8.13.0-amd64.deb ]]; then
+    curl -L -o /tmp/logstash-8.13.0-amd64.deb https://artifacts.elastic.co/downloads/logstash/logstash-8.13.0-amd64.deb
+fi
+
+
+dpkg -i /tmp/logstash-8.13.0-amd64.deb
+rm -f /tmp/logstash-8.13.0-amd64.deb
+
+
+
+exit 0
+
+
 # https://www.elastic.co/guide/en/logstash/8.12/installing-logstash.html#_apt
 
 export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
 export NEEDRESTART_SUSPEND=1
+
+
+
+
+
+
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
 apt-get --assume-yes install apt-transport-https
