806dfd1c4a72055a0b415043cbd0902654d241c1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-28 12:25:15 -0700

    Fix Unable to locate package elasticsearch

--- install-logstash-on-ubuntu.sh
+++ install-logstash-on-ubuntu.sh
@@ -30,16 +30,6 @@ export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
 export NEEDRESTART_SUSPEND=1
 
-
-
-
-
-
-
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/elastic-keyring.gpg
-apt-get --assume-yes install apt-transport-https
-echo "deb [signed-by=/usr/share/keyrings/elastic-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-8.x.list
-
 apt-get update
 
 apt-get --fix-missing --assume-yes install logstash
