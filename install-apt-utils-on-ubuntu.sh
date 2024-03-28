0b2901052ec5d60a02eefee8a175be9bdff7a7e3
Taylor Monacelli <taylormonacelli@gmail.com>
2024-03-28 14:36:24 -0700

    apt-utils

--- install-apt-utils-on-ubuntu.sh
+++ install-apt-utils-on-ubuntu.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-
 # https://github.com/moby/moby/issues/27988
 
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get install --assume-yes apt-utils
+DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes apt-utils dialog
