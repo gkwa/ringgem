35c87ddb6b30d972c6b1c4f94d25332af770d5f0
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-01 16:01:21 -0700

    Add go-internal for txtar-x support

---
+++ install-txtar-on-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# provides:
+# /usr/bin/testscript
+# /usr/bin/txtar-addmod
+# /usr/bin/txtar-c
+# /usr/bin/txtar-goproxy
+# /usr/bin/txtar-x
+
+apt-get --assume-yes install go-internal
