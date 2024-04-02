58711c5ec57e29387dcc2c459e60ef9405d6f70e
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-02 07:22:50 -0700

    Add homebrew on ubuntu

---
+++ install-homebrew-on-ubuntu.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# https://docs.brew.sh/Homebrew-on-Linux
+
+sudo useradd --create-home linuxbrew --shell /bin/bash
+echo "linuxbrew ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/linuxbrew
+
+sudo apt-get install --assume-yes build-essential procps curl file git gcc
+
+
+
+sudo su - linuxbrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
+source ~/.bashrc
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/linuxbrew/.profile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
