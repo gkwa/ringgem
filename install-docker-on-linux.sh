d7dfe22d4c7dbe3868af64e000672da948657561
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-03 16:37:38 -0700

    adjust docker-on-linux

--- install-docker-on-linux.sh
+++ install-docker-on-linux.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-if grep --quiet --extended-regexp 'ID(_LIKE)?=.*\b(centos|fedora|rhel|amazon)\b' /etc/os-release; then
-    yum --assumeyes install docker
-else
-    curl -fsSL https://get.docker.com/ | sh
-fi
-
+curl -fsSL https://get.docker.com/ | sh
 systemctl start docker.service
 docker run hello-world
