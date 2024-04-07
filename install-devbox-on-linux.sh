ba43b2cfa5c698d6a6ac6c737c58ecdd8a728dd6
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-07 16:17:47 -0700

    seed devbox packages

--- install-devbox-on-linux.sh
+++ install-devbox-on-linux.sh
@@ -7,18 +7,20 @@ curl -fsSL https://get.jetpack.io/devbox | FORCE=1 bash
 
 # cache commonly used stuff to make next run faster
 dir=$(mktemp -d /tmp/devbox-XXXX)
+cd $dir
 devbox init $dir
-devbox add awscli2
 devbox add git
 devbox add ansible
 devbox add docker
 devbox add go
 devbox add python3
+devbox add awscli2
 devbox install
 devbox shell
 aws --version
 
 
+cd
 
 
 rm -rf $dir
