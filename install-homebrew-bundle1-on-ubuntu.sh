d3951f999b730b811247dc77fbb77db55713c8cd
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-10 15:19:48 -0700

    Add brew bundle--this is bad idea

---
+++ install-homebrew-bundle1-on-ubuntu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# https://github.com/Homebrew/homebrew-bundle?tab=readme-ov-file#homebrew-bundle
+
+cat >/home/linuxbrew/Brewfile <<'EOF'
+tap "carvel-dev/carvel"
+brew "ytt"
+EOF
+
+
+chmod a+r /home/linuxbrew/Brewfile
+sudo --login --user linuxbrew brew bundle install
