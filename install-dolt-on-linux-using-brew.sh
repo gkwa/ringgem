cf1d8fcfcd9968baf2c309f086682552d4f4e74b
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-13 23:39:43 -0400

    dolt

--- install-dolt-on-linux-using-brew.sh
+++ install-dolt-on-linux-using-brew.sh
@@ -4,4 +4,13 @@ if [[ $(uname) != "Linux" ]]; then
     echo "failed to install dolt"
 fi
 
+set +e
 sudo --login --user linuxbrew brew install dolt
+set -e
+
+set -o | grep xtrace | cut -f2 -d- | read orig_xtrace
+set +x
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [[ "$orig_xtrace" == "off" ]]; then set +x; else set -x; fi
+
+dolt version
