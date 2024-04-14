a7d36c7a1ef741f99ca932258199a0b7bde1d3c1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-13 23:22:25 -0400

    clusterawsadm

--- install-clusterawsadm-on-linux.sh
+++ install-clusterawsadm-on-linux.sh
@@ -4,5 +4,10 @@ set +e
 sudo --login --user linuxbrew brew install clusterawsadm
 set -e
 
+
+set -o | grep xtrace | cut -f2 -d- | read orig_xtrace
+set +x
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [[ "$orig_xtrace" == "off" ]]; then set +x; else set -x; fi
+
 clusterawsadm version
