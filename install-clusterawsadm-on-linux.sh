e3b15471aaed6bbb635f37964162d6d163a65c4c
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-13 22:29:41 -0400

    clusterawsadm

--- install-clusterawsadm-on-linux.sh
+++ install-clusterawsadm-on-linux.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-if grep --silent 'Amazon Linux' <<<"$(grep ^NAME= /etc/os-release)"; then
-    brew install clusterawsadm
-    return
-fi
-
-version=$(curl -s https://api.github.com/repos/kubernetes-sigs/cluster-api-provider-aws/releases/latest | grep tag_name | cut -d '"' -f 4)
-curl -Lo clusterawsadm https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/$version/clusterawsadm-linux-amd64
-chmod +x clusterawsadm
-sudo mv clusterawsadm /usr/local/bin/clusterawsadm
-clusterawsadm version
+sudo --login --user linuxbrew brew install clusterawsadm
