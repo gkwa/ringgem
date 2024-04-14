0aa3e2e7b72403af05aa6850d7d194f60412c6fd
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-13 23:16:08 -0400

    clusterawsadm

--- install-clusterawsadm-on-linux.sh
+++ install-clusterawsadm-on-linux.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-set +e
 sudo --login --user linuxbrew brew install clusterawsadm
-sudo --login --user linuxbrew brew install clusterawsadm
-set -e
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 clusterawsadm version
