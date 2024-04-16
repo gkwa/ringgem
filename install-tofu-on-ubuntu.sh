be13846aad1f5e0c6d9255df3a4c2c215892274a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-15 22:45:32 -0400

    Fix tofu

--- install-tofu-on-ubuntu.sh
+++ install-tofu-on-ubuntu.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
+set -u
 
 sudo --login --user linuxbrew brew install opentofu
+
+set -o | grep xtrace | cut -f2 -d- | read orig_xtrace
+set +x
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+if [[ "$orig_xtrace" == "off" ]]; then set +x; else set -x; fi
 
 tofu version >/dev/null
