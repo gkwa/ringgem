b7a1ead783ade6a4732c3ff56ab1f541a45df558
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-16 23:01:35 -0400

    Add eachit

--- install-eachit-on-linux.sh
+++ install-eachit-on-linux.sh
@@ -3,8 +3,9 @@
 url=https://github.com/taylormonacelli/eachit/releases/latest/download/eachit_Linux_x86_64.tar.gz
 checksums_url=https://github.com/taylormonacelli/eachit/releases/latest/download/checksums.txt
 
-pushd=$(mktemp -d /tmp/eachit-XXXX)
-cd $dir
+tmp=$(mktemp -d /tmp/eachit-XXXX)
+orig="$(pwd)"
+cd $tmp
 
 curl --fail --silent --show-error --location --output eachit_Linux_x86_64.tar.gz $url
 curl --fail --silent --show-error --location --output checksums.txt $checksums_url
@@ -17,6 +18,6 @@ else
     exit 1
 fi
 
-popd
-rm -rf $dir
+cd $orig
+rm -rf $tmp
 eachit --help
