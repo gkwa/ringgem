416c866f09d44bb002ace600b4a453df9e105561
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-17 09:02:56 -0400

    Add git

--- install-git-on-linux.sh
+++ install-git-on-linux.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 if command -v apt-get &>/dev/null; then
-   DEBIAN_FRONTEND=noninteractive apt-get -y install git
+    DEBIAN_FRONTEND=noninteractive apt-get -y install git
 elif command -v yum &>/dev/null; then
-   yum -y install git
+    yum -y install git
 elif command -v dnf &>/dev/null; then
-   dnf -y install git
+    dnf -y install git
 elif command -v zypper &>/dev/null; then
-   zypper -n install git
+    zypper -n install git
 elif command -v apk &>/dev/null; then
-   apk add --no-cache git
+    apk add --no-cache git
 elif command -v pkg &>/dev/null; then
-   pkg install -y git
+    pkg install -y git
 elif command -v pacman &>/dev/null; then
-   pacman -Sy --noconfirm git
+    pacman -Sy --noconfirm git
 else
-   echo "No supported package manager is available."
-   exit 1
+    echo "No supported package manager is available."
+    exit 1
 fi
