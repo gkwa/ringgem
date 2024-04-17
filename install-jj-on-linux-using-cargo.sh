8a743f85a4b51d0469b14d93da57054ef8149421
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-17 13:50:25 -0400

    Add jj install using cargo

--- install-jj-on-linux-using-cargo.sh
+++ install-jj-on-linux-using-cargo.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+. "$HOME/.cargo/env"
+
+export PATH=/root/.cargo/bin:$PATH
+
+
+
 # https://github.com/cargo-bins/cargo-binstall?tab=readme-ov-file#quickly
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
