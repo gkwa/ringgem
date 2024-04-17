21f067be5f5ba02799628c7514336f38a3e6f679
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-17 19:11:51 -0400

    remove apps from devbox install

--- install-devbox-on-linux.sh
+++ install-devbox-on-linux.sh
@@ -1,21 +1,3 @@
 #!/usr/bin/env bash
 
 curl -fsSL https://get.jetpack.io/devbox | FORCE=1 bash
-
-# cache commonly used stuff to make next run faster
-dir=$(mktemp -d /tmp/devbox-XXXX)
-cd $dir
-devbox init $dir
-devbox add git
-devbox add awscli2
-devbox add ansible
-devbox add docker
-devbox add go
-devbox add python3
-devbox install
-devbox shell
-aws --version
-
-cd
-
-rm -rf $dir
