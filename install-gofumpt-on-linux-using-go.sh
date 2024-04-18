03a2187d2e9b4f10bb4996e222736cead0f51f32
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-17 23:26:33 -0400

    go env GOPATH

--- install-gofumpt-on-linux-using-go.sh
+++ install-gofumpt-on-linux-using-go.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH:$(go env GOPATH)/bin
+export PATH=$PATH:$(/usr/local/go/bin/go env GOPATH)/bin
 
 /usr/local/go/bin/go install mvdan.cc/gofumpt@latest
 
--- install-golangci-lint-on-linux-using-go.sh
+++ install-golangci-lint-on-linux-using-go.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH:$(go env GOPATH)/bin
+export PATH=$PATH:$(/usr/local/go/bin/go env GOPATH)/bin
 
 /usr/local/go/bin/go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
 
