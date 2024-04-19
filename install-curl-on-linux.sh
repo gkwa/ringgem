1baa159719c18774ecd981837c1ff084bd191b41
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-18 22:13:05 -0400

    trivial check

--- install-curl-on-linux.sh
+++ install-curl-on-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if command -v apt &>/dev/null; then
+if command -v apt-get &>/dev/null; then
     apt-get --yes install curl
 elif command -v yum &>/dev/null; then
     yum -y install curl
