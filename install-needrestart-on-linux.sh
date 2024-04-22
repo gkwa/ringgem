2de049c026661e16aae3eaa6f62c8d2f3916aae3
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-21 20:40:46 -0400

    tweak

--- install-needrestart-on-linux.sh
+++ install-needrestart-on-linux.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-if command -v apt-get &> /dev/null; then
-   sudo apt-get update
-   sudo apt-get install -y needrestart
-elif command -v dnf &> /dev/null; then
-   sudo dnf install -y needrestart
-elif command -v yum &> /dev/null; then
-   sudo yum install -y epel-release
-   sudo yum install -y needrestart
-elif command -v zypper &> /dev/null; then
-   sudo zypper refresh
-   sudo zypper install -y needrestart
-elif command -v pacman &> /dev/null; then
-   sudo pacman -Sy needrestart
+if command -v apt-get &>/dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y needrestart
+elif command -v dnf &>/dev/null; then
+    sudo dnf install -y needrestart
+elif command -v yum &>/dev/null; then
+    sudo yum install -y epel-release
+    sudo yum install -y needrestart
+elif command -v zypper &>/dev/null; then
+    sudo zypper refresh
+    sudo zypper install -y needrestart
+elif command -v pacman &>/dev/null; then
+    sudo pacman -Sy needrestart
 else
-   echo "Unsupported distribution. Please install needrestart manually."
-   exit 1
+    echo "Unsupported distribution. Please install needrestart manually."
+    exit 1
 fi
 
 echo "needrestart has been successfully installed."
--- install-otel-collector-on-linux.sh
+++ install-otel-collector-on-linux.sh
@@ -8,13 +8,13 @@ set -u
 version=$(curl -sSL https://api.github.com/repos/open-telemetry/opentelemetry-collector-releases/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
 
 if [ -f /etc/debian_version ]; then
-  apt-get install -y wget
-  wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/${version}/otelcol_${version#v}_linux_amd64.deb
-  apt-get install -y ./otelcol_${version#v}_linux_amd64.deb
+    apt-get install -y wget
+    wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/${version}/otelcol_${version#v}_linux_amd64.deb
+    apt-get install -y ./otelcol_${version#v}_linux_amd64.deb
 elif [ -f /etc/redhat-release ]; then
-  yum -y install wget
-  wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/${version}/otelcol_${version#v}_linux_amd64.rpm
-  yum install -y ./otelcol_${version#v}_linux_amd64.rpm
+    yum -y install wget
+    wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/${version}/otelcol_${version#v}_linux_amd64.rpm
+    yum install -y ./otelcol_${version#v}_linux_amd64.rpm
 fi
 
 otelcol --version
