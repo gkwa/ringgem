e6a1cbc3126a6942ffa172be175e8ccc7a393974
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-21 20:45:06 -0400

    tweak

--- install-otel-collector-on-linux.sh
+++ install-otel-collector-on-linux.sh
@@ -8,11 +8,9 @@ set -u
 version=$(curl -sSL https://api.github.com/repos/open-telemetry/opentelemetry-collector-releases/releases/latest | grep -Eo '"tag_name": "\K[^"]+'
 
 if [ -f /etc/debian_version ]; then
-   apt-get install -y curl
    curl -sSL -o otelcol_${version#v}_linux_amd64.deb https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/${version}/otelcol_${version#v}_linux_amd64.deb
    apt-get install -y ./otelcol_${version#v}_linux_amd64.deb
 elif [ -f /etc/redhat-release ]; then
-   yum -y install curl
    curl -sSL -o otelcol_${version#v}_linux_amd64.rpm https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/${version}/otelcol_${version#v}_linux_amd64.rpm
    yum install -y ./otelcol_${version#v}_linux_amd64.rpm
 fi
