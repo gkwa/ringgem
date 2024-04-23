1493f55c787eaf92f4ff23c6b7a08340fe4e7e09
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-23 09:17:47 -0400

    install jq without package manager

--- install-jq-on-linux.sh
+++ install-jq-on-linux.sh
@@ -1,11 +1,69 @@
 #!/usr/bin/env bash
 
-if command -v apt-get &>/dev/null; then
-    DEBIAN_FRONTEND=noninteractive apt-get -y install jq
-elif command -v yum &>/dev/null; then
-    yum -y install jq
+function install_jq_without_pm {
+    version=$(curl -s https://api.github.com/repos/jqlang/jq/releases/latest | grep tag_name | cut -d'"' -f4)
+    kernel=$(uname)
+
+    if [[ $kernel == Darwin ]]; then
+        if [[ $(uname -m) == arm64 ]]; then
+            binary=jq-macos-arm64
+        else
+            binary=jq-macos-amd64
+        fi
+    elif [[ $kernel == Linux ]]; then
+        if [[ $(uname -m) == x86_64 ]]; then
+            binary=jq-linux-amd64
+        elif [[ $(uname -m) == aarch64 ]]; then
+            binary=jq-linux-arm64
+        elif [[ $(uname -m) == armv7l ]]; then
+            binary=jq-linux-armhf
+        elif [[ $(uname -m) == i686 ]]; then
+            binary=jq-linux-i386
+        else
+            echo Unsupported architecture: $(uname -m)
+            exit 1
+        fi
+    else
+        echo Unsupported operating system: $kernel
+        exit 1
+    fi
+
+    echo Installing jq $version for $kernel $(uname -m)
+
+    curl -Lo /tmp/$binary https://github.com/jqlang/jq/releases/download/$version/$binary
+    install --mode 0755 --owner root --group root /tmp/$binary /usr/local/bin/jq
+    rm /tmp/$binary
+
+    jq --version
+}
+
+function install_jq_with_pm {
+    if command -v apt-get &>/dev/null; then
+        sudo apt-get update
+        sudo apt-get install -y jq
+    elif command -v yum &>/dev/null; then
+        sudo yum install -y jq
+    elif command -v dnf &>/dev/null; then
+        sudo dnf install -y jq
+    elif command -v pacman &>/dev/null; then
+        sudo pacman -Sy jq
+    elif command -v zypper &>/dev/null; then
+        sudo zypper install -y jq
+    elif command -v brew &>/dev/null; then
+        brew install jq
+    else
+        echo "No supported package manager found. Please install jq manually."
+        exit 1
+    fi
+
+}
+
+use_pm=true
+
+if $use_pm; then
+    install_jq_with_pm
 else
-    echo "Neither apt nor yum package manager is available."
+    install_jq_without_pm
 fi
 
 jq --version >/dev/null
