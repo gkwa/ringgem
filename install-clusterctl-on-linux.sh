3f313f8d57e5c40072ff7abf1b40d4c6ba40958b
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-23 09:30:43 -0400

    fix otel-collector installer

--- install-clusterctl-on-linux.sh
+++ install-clusterctl-on-linux.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-version=$(curl -s https://api.github.com/repos/kubernetes-sigs/cluster-api/releases/latest | grep tag_name | cut -d '"' -f 4)
+version=$(curl -s https://api.github.com/repos/kubernetes-sigs/cluster-api/releases/latest | jq -r .tag_name)
 curl -Lo clusterctl https://github.com/kubernetes-sigs/cluster-api/releases/download/$version/clusterctl-linux-amd64
+
 install -o root -g root -m 0755 clusterctl /usr/local/bin/clusterctl
+
 rm -f clusterctl
+
 clusterctl version
