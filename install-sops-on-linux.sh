82d5c444a9e6f012866015435f27d606a60b731c
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-23 17:59:30 -0400

    fix-sops

--- install-sops-on-linux.sh
+++ install-sops-on-linux.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 version=$(curl -fsSL https://api.github.com/repos/getsops/sops/releases/latest | jq -r '.tag_name')
-url=https://github.com/mozilla/sops/releases/download/${version}/sops-${version#v}.linux.amd64
-rm -f /tmp/sops
-curl -fsSLo /tmp/sops $url
-install --mode 0755 --group root --owner root /tmp/sops /usr/local/bin/sops
+url=https://github.com/mozilla/sops/releases/download/${version}/sops-${version}.linux.amd64
+
+sopsdir=$(mkptem -d /tmp/sops-XXXX)
+
+curl -fsSLo $sopsdir/sops $url
+install --mode 0755 --group root --owner root $sopsdir/sops /usr/local/bin/sops
+
 sops --version
 
-rm /tmp/sops
+rm -rf $sopsdir
