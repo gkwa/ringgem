424f731cf326348db3d26be8b78eb1ea7edbe5f5
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-23 09:44:34 -0400

    cnquery

--- install-cnspec-on-ubuntu.sh
+++ install-cnspec-on-ubuntu.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-version=$(curl --silent "https://api.github.com/repos/mondoohq/cnquery/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | tr -d v) &&
-    url="https://github.com/mondoohq/cnquery/releases/download/v${version}/cnquery_${version}_linux_amd64.deb" &&
-    echo $url &&
-    curl -LO $url &&
-    dpkg -i cnquery_${version}_linux_amd64.deb &&
-    cnquery version
+version=$(curl -s https://api.github.com/repos/mondoohq/cnquery/releases/latest | jq -r '.tag_name')
+url="https://github.com/mondoohq/cnquery/releases/download/${version}/cnquery_${version#v}_linux_amd64.deb"
+curl -LO $url
+dpkg -i cnquery_${version#v}_linux_amd64.deb
 
-version=$(curl --silent "https://api.github.com/repos/mondoohq/cnspec/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' | tr -d v) &&
-    url="https://github.com/mondoohq/cnspec/releases/download/v${version}/cnspec_${version}_linux_amd64.deb" &&
-    echo $url &&
-    curl -LO $url &&
-    dpkg -i cnspec_${version}_linux_amd64.deb &&
-    cnspec version
+version=$(curl -s https://api.github.com/repos/mondoohq/cnspec/releases/latest | jq -r '.tag_name')
+url="https://github.com/mondoohq/cnspec/releases/download/${version}/cnspec_${version#v}_linux_amd64.deb"
+curl -LO $url
+dpkg -i cnspec_${version#v}_linux_amd64.deb
+
+cnquery version
+cnspec version
+
+rm -f cnquery_${version#v}_linux_amd64.deb
+rm -f cnspec_${version#v}_linux_amd64.deb
