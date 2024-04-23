7a26fd140a343f58cd147367ab062f1d4a7e5cc8
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-23 13:07:55 -0400

    tweak

--- install-unzip-on-linux copy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if command -v apt &>/dev/null; then
-    DEBIAN_FRONTEND=noninteractive apt-get -y install unzip
-elif command -v yum &>/dev/null; then
-    yum -y install unzip
-else
-    echo "Neither apt nor yum package manager is available."
-fi
