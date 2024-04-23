223fbb6a0071c4275922c268c9f75f1b9e5007e9
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-23 13:24:56 -0400

    tweak

--- install-jless-on-ubuntu.sh
+++ install-jless-on-ubuntu.sh
@@ -7,8 +7,7 @@ apt-get -y install \
 
 rm -f /tmp/jless
 version=$(curl -s https://api.github.com/repos/PaulJuliusMartinez/jless/releases/latest | jq -r '.tag_name')
-
-zip=jless-${version#v}-x86_64-unknown-linux-gnu.zip
+zip=jless-${version}-x86_64-unknown-linux-gnu.zip
 
 curl -LO https://github.com/PaulJuliusMartinez/jless/releases/download/${version}/$zip
 unzip -o $zip -d /tmp/
