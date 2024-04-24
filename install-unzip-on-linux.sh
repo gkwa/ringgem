91eef36563ed2f3a6db3e7c6d79588a0c0ca6ca0
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-24 12:30:52 -0400

    Revert "zip"
    This reverts commit ba5e35b94dde0e65ceeaf2370d7a472072c16157.

--- install-unzip-on-linux.sh
+++ install-unzip-on-linux.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
 if command -v apt-get &>/dev/null; then
-    apt-get install -y unzip zip
+    apt-get install -y unzip
 elif command -v dnf &>/dev/null; then
-    dnf install -y unzip zip
+    dnf install -y unzip
 elif command -v yum &>/dev/null; then
-    yum install -y unzip zip
+    yum install -y unzip
 elif command -v zypper &>/dev/null; then
-    zypper install -y unzip zip
+    zypper install -y unzip
 elif command -v apk &>/dev/null; then
-    apk add --no-cache unzip zip
+    apk add --no-cache unzip
 elif command -v pacman &>/dev/null; then
-    pacman -Sy --noconfirm unzip zip
+    pacman -Sy --noconfirm unzip
 else
-    echo "Unsupported package manager. Please install zip and unzip manually."
+    echo "Unsupported package manager. Please install unzip manually."
     exit 1
 fi
 
 unzip -v
-zip -?
