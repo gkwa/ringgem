b0a470e5b27255bc7cfabb2d81942b6f2295460a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-24 10:03:22 -0400

    twerking

--- install-awscli-on-linux.sh
+++ install-awscli-on-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if command -v apt &>/dev/null; then
+if command -v apt-get &>/dev/null; then
     DEBIAN_FRONTEND=noninteractive apt-get -y install awscli
 elif command -v yum &>/dev/null; then
     yum -y install awscli
