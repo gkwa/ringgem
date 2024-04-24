ba5e35b94dde0e65ceeaf2370d7a472072c16157
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-24 12:29:23 -0400

    zip

--- install-unzip-on-linux.sh
+++ install-unzip-on-linux.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 if command -v apt-get &>/dev/null; then
-    apt-get install -y unzip
+    apt-get install -y unzip zip
 elif command -v dnf &>/dev/null; then
-    dnf install -y unzip
+    dnf install -y unzip zip
 elif command -v yum &>/dev/null; then
-    yum install -y unzip
+    yum install -y unzip zip
 elif command -v zypper &>/dev/null; then
-    zypper install -y unzip
+    zypper install -y unzip zip
 elif command -v apk &>/dev/null; then
-    apk add --no-cache unzip
+    apk add --no-cache unzip zip
 elif command -v pacman &>/dev/null; then
-    pacman -Sy --noconfirm unzip
+    pacman -Sy --noconfirm unzip zip
 else
-    echo "Unsupported package manager. Please install unzip manually."
+    echo "Unsupported package manager. Please install zip and unzip manually."
     exit 1
 fi
 
 unzip -v
+zip -?
