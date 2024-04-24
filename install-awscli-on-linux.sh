a19180923a5a2c68a6f70d9a259c16f37b182af2
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-24 10:06:55 -0400

    twerking

--- install-awscli-on-linux.sh
+++ install-awscli-on-linux.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 if command -v apt-get &>/dev/null; then
- for i in {1..5}; do
-   DEBIAN_FRONTEND=noninteractive apt-get -y install awscli && break
-   sleep 1
- done
+    for i in {1..5}; do
+        DEBIAN_FRONTEND=noninteractive apt-get -y install awscli && break
+        sleep 1
+    done
 elif command -v yum &>/dev/null; then
- for i in {1..5}; do
-   yum -y install awscli && break
-   sleep 1
- done
+    for i in {1..5}; do
+        yum -y install awscli && break
+        sleep 1
+    done
 else
- echo "Neither apt nor yum package manager is available."
+    echo "Neither apt nor yum package manager is available."
 fi
