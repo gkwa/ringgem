33708e8818f16c9a6cc47eb5d8c5f0b1601c7a0a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-24 10:43:52 -0400

    awscli

--- install-awscli-on-linux.sh
+++ install-awscli-on-linux.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
-if command -v apt-get &>/dev/null; then
-    orig_dir=$(pwd)
-    tmp=$(mktemp -d awscli-XXXX)
-    cd $tmp
-    curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
-    unzip awscliv2.zip
-    sudo ./aws/install
-    cd $orig_dir
-fi
-
-if command -v yum &>/dev/null; then
-    for i in {1..5}; do
-        yum -y install awscli && break
-        sleep 1
-    done
-fi
+orig_dir=$(pwd)
+tmp=$(mktemp -d awscli-XXXX)
+cd $tmp
+curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
+unzip awscliv2.zip
+sudo ./aws/install
+cd $orig_dir
 
 aws --version
 
