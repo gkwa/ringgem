567c23ab3498de8f6be2bd104e1143fc60f51c50
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-25 17:57:43 -0400

    twurking

--- install-envsubst-on-linux.sh
+++ install-envsubst-on-linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -f /tmp/envsubst
-curl -L https://github.com/a8m/envsubst/releases/latest/download/envsubst-$(uname -s)-$(uname -m) -o /tmp/envsubst
+curl -o /tmp/envsubst -L https://github.com/a8m/envsubst/releases/latest/download/envsubst-$(uname -s)-$(uname -m)
 install --mode 0755 --group root --owner root /tmp/envsubst /usr/local/bin/envsubst
 
 envsubst --version >/dev/null
