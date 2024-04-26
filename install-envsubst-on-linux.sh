0e9246bbf101a271467ddb99a9c8d1c7657f0eda
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-25 20:20:34 -0400

    fiddle

--- install-envsubst-on-linux.sh
+++ install-envsubst-on-linux.sh
@@ -3,6 +3,8 @@
 rm -f /tmp/envsubst
 curl -o /tmp/envsubst -L https://github.com/a8m/envsubst/releases/latest/download/envsubst-$(uname -s)-$(uname -m)
 
+chmod +x /tmp/envsubst
+
 /tmp/envsubst --version >/dev/null
 
 install --mode 0755 --group root --owner root /tmp/envsubst /usr/local/bin/envsubst
