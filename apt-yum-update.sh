8cca4b307a55c21dbf29bba9b062d7f4569ae72f
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-26 15:02:55 -0400

    retrun

--- apt-yum-update.sh
+++ apt-yum-update.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
-[[ ! -f /etc/os-release ]] && exit 1
+max_attempts=5
+attempt=1
 
-if grep --silent Ubuntu <<<"$(grep ^NAME= /etc/os-release)"; then
+while [[ $attempt -le $max_attempts ]]; do
+  echo "Attempt $attempt:"
+
+  [[ ! -f /etc/os-release ]] && exit 1
+
+  if grep --silent Ubuntu <<<"$(grep ^NAME= /etc/os-release)"; then
     echo Running apt-get update...
-    sudo DEBIAN_FRONTEND=noninteractive apt-get update
-fi
+    if sudo DEBIAN_FRONTEND=noninteractive apt-get update; then
+      echo "apt-get update succeeded. Exiting."
+      exit 0
+    else
+      echo "apt-get update failed. Retrying."
+    fi
+  fi
+
+  attempt=$((attempt + 1))
+  echo "Waiting for 5 seconds before the next attempt..."
+  sleep 5
+done
+
+echo "All attempts failed. Exiting with an error."
+exit 1
