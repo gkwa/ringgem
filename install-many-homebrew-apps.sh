d4dc209efe4230115a9adb17d67b1ba780329e80
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-26 15:10:48 -0400

    retry

--- install-many-homebrew-apps.sh
+++ install-many-homebrew-apps.sh
@@ -2,48 +2,32 @@
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-packages=(
-  carvel-dev/carvel/ytt
-  clusterawsadm
-  clusterctl
-  dolt
-  gcc
-  gofumpt
-  golangci/tap/golangci-lint
-  helm
-  jj
-  jless
-  k9s
-  kcl-lang/tap/kcl
-  kind
-  nvim
-  opentofu
-  ripgrep
-  terragrunt
+declare -A packages=(
+  ["carvel-dev/carvel/ytt"]="ytt version"
+  ["clusterawsadm"]="clusterawsadm version"
+  ["clusterctl"]="clusterctl version"
+  ["dolt"]="dolt version"
+  ["gcc"]="gcc --version"
+  ["gofumpt"]="gofumpt --version"
+  ["golangci/tap/golangci-lint"]="golangci-lint --version"
+  ["helm"]="helm version"
+  ["jj"]="jj --version"
+  ["jless"]="jless --version"
+  ["k9s"]="k9s version"
+  ["kcl-lang/tap/kcl"]="kcl version"
+  ["kind"]="kind --version"
+  ["nvim"]="nvim --version"
+  ["opentofu"]="tofu version"
+  ["ripgrep"]="rg --version"
+  ["terragrunt"]="terragrunt --version"
 )
 
-for package in "${packages[@]}"; do
+for package in "${!packages[@]}"; do
   if ! brew list "$package" >/dev/null 2>&1; then
     sudo --user linuxbrew --login brew install "$package"
   fi
 done
 
-{
-  clusterawsadm version
-  clusterctl version
-  dolt version
-  gcc --version
-  gofumpt --version
-  golangci-lint --version
-  helm version
-  jj --version
-  jless --version
-  k9s version
-  kcl version
-  kind --version
-  nvim --version
-  rg --version
-  terragrunt --version
-  tofu version
-  ytt --version
-} >/dev/null
+for version_command in "${packages[@]}"; do
+  eval "$version_command" >/dev/null
+done
