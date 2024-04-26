3b5f808d843362e99f33ddc778eb441bedd24076
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-26 15:07:43 -0400

    idempoten_brew

--- install-many-homebrew-apps.sh
+++ install-many-homebrew-apps.sh
@@ -2,42 +2,48 @@
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-sudo --user linuxbrew --login brew install \
-    carvel-dev/carvel/ytt \
-    clusterawsadm \
-    clusterctl \
-    dolt \
-    gcc \
-    gofumpt \
-    golangci/tap/golangci-lint \
-    helm \
-    jj \
-    jless \
-    k9s \
-    kcl-lang/tap/kcl \
-    kind \
-    nvim \
-    opentofu \
-    ripgrep \
-    terragrunt
+packages=(
+  carvel-dev/carvel/ytt
+  clusterawsadm
+  clusterctl
+  dolt
+  gcc
+  gofumpt
+  golangci/tap/golangci-lint
+  helm
+  jj
+  jless
+  k9s
+  kcl-lang/tap/kcl
+  kind
+  nvim
+  opentofu
+  ripgrep
+  terragrunt
+)
+
+for package in "${packages[@]}"; do
+  if ! brew list "$package" >/dev/null 2>&1; then
+    sudo --user linuxbrew --login brew install "$package"
+  fi
+done
 
 {
-    clusterawsadm version
-    clusterctl version
-    dolt version
-    gcc --version
-    gofumpt --version
-    golangci-lint --version
-    helm version
-    jj --version
-    jless --version
-    k9s version
-    kcl version
-    kind --version
-    nvim --version
-    rg --version
-    terragrunt --version
-    tofu version
-    ytt --version
-
+  clusterawsadm version
+  clusterctl version
+  dolt version
+  gcc --version
+  gofumpt --version
+  golangci-lint --version
+  helm version
+  jj --version
+  jless --version
+  k9s version
+  kcl version
+  kind --version
+  nvim --version
+  rg --version
+  terragrunt --version
+  tofu version
+  ytt --version
 } >/dev/null
