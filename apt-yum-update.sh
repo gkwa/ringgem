a661e286db0f06d2a78f7367b62c7d9d613f9257
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-28 08:28:16 -0400

    tweak

--- apt-yum-update.sh
+++ apt-yum-update.sh
@@ -4,23 +4,23 @@ max_attempts=5
 attempt=1
 
 while [[ $attempt -le $max_attempts ]]; do
-  echo "Attempt $attempt:"
+    echo "Attempt $attempt:"
 
-  [[ ! -f /etc/os-release ]] && exit 1
+    [[ ! -f /etc/os-release ]] && exit 1
 
-  if grep --silent Ubuntu <<<"$(grep ^NAME= /etc/os-release)"; then
-    echo Running apt-get update...
-    if sudo DEBIAN_FRONTEND=noninteractive apt-get update; then
-      echo "apt-get update succeeded. Exiting."
-      exit 0
-    else
-      echo "apt-get update failed. Retrying."
+    if grep --silent Ubuntu <<<"$(grep ^NAME= /etc/os-release)"; then
+        echo Running apt-get update...
+        if sudo DEBIAN_FRONTEND=noninteractive apt-get update; then
+            echo "apt-get update succeeded. Exiting."
+            exit 0
+        else
+            echo "apt-get update failed. Retrying."
+        fi
     fi
-  fi
 
-  attempt=$((attempt + 1))
-  echo "Waiting for 5 seconds before the next attempt..."
-  sleep 5
+    attempt=$((attempt + 1))
+    echo "Waiting for 5 seconds before the next attempt..."
+    sleep 5
 done
 
 echo "All attempts failed. Exiting with an error."
--- install-many-homebrew-apps.sh
+++ install-many-homebrew-apps.sh
@@ -3,40 +3,40 @@
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 declare -A packages=(
-  ["awscli@2"]="aws --version"
-  ["bash-completion@2"]=""
-  ["carvel-dev/carvel/ytt"]="ytt version"
-  ["clusterawsadm"]="clusterawsadm version"
-  ["clusterctl"]="clusterctl version"
-  ["dolt"]="dolt version"
-  ["gcc"]="gcc --version"
-  ["gofumpt"]="gofumpt --version"
-  ["golangci/tap/golangci-lint"]="golangci-lint --version"
-  ["helm"]="helm version"
-  ["jj"]="jj --version"
-  ["jless"]="jless --version"
-  ["k9s"]="k9s version"
-  ["kcl-lang/tap/kcl"]="kcl version"
-  ["kind"]="kind --version"
-  ["nvim"]="nvim --version"
-  ["opentofu"]="tofu version"
-  ["packer-completion"]=""
-  ["packer"]="packer --version"
-  ["restic"]="restic version"
-  ["ripgrep"]="rg --version"
-  ["taylormonacelli/homebrew-tools/cloudelf"]="cloudelf --help"
-  ["terragrunt"]="terragrunt --version"
+    ["awscli@2"]="aws --version"
+    ["bash-completion@2"]=""
+    ["carvel-dev/carvel/ytt"]="ytt version"
+    ["clusterawsadm"]="clusterawsadm version"
+    ["clusterctl"]="clusterctl version"
+    ["dolt"]="dolt version"
+    ["gcc"]="gcc --version"
+    ["gofumpt"]="gofumpt --version"
+    ["golangci/tap/golangci-lint"]="golangci-lint --version"
+    ["helm"]="helm version"
+    ["jj"]="jj --version"
+    ["jless"]="jless --version"
+    ["k9s"]="k9s version"
+    ["kcl-lang/tap/kcl"]="kcl version"
+    ["kind"]="kind --version"
+    ["nvim"]="nvim --version"
+    ["opentofu"]="tofu version"
+    ["packer-completion"]=""
+    ["packer"]="packer --version"
+    ["restic"]="restic version"
+    ["ripgrep"]="rg --version"
+    ["taylormonacelli/homebrew-tools/cloudelf"]="cloudelf --help"
+    ["terragrunt"]="terragrunt --version"
 )
 
 for package in "${!packages[@]}"; do
-  if ! brew list "$package" >/dev/null 2>&1; then
-    sudo --user linuxbrew --login brew install "$package"
-  fi
+    if ! brew list "$package" >/dev/null 2>&1; then
+        sudo --user linuxbrew --login brew install "$package"
+    fi
 done
 
 for package in "${!packages[@]}"; do
- version_command="${packages[$package]}"
- if [[ -n "$version_command" ]]; then
-   eval "$version_command" >/dev/null
- fi
+    version_command="${packages[$package]}"
+    if [[ -n $version_command ]]; then
+        eval "$version_command"
+    fi
 done
