2575ab7a24f19fab95201430991cf7c2c54c73c9
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-28 22:56:47 -0400

    move to homebrew

--- install-apt-utils-on-ubuntu.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# https://github.com/moby/moby/issues/27988
-
-DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes apt-utils dialog
--- install-java-jre-on-ubuntu.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-DEBIAN_FRONTEND=noninteractive sudo apt-get --assume-yes install default-jre
