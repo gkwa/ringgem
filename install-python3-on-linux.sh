8c4bbba11ff9dbde63ef64427900a1b75a15ce78
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-30 07:10:07 -0700

    pip3

--- install-python3-on-linux.sh
+++ install-python3-on-linux.sh
@@ -3,7 +3,6 @@
 if [ -f /etc/fedora-release ]; then
     dnf install -y python3
 elif [ -f /etc/lsb-release ] || [ -f /etc/debian_version ]; then
-    apt-get update
     apt-get install -y python3
 else
     echo "Unsupported distribution"
@@ -17,6 +16,5 @@ python3 -m ensurepip --default-pip
 python3 -m pip install --upgrade pip setuptools wheel
 
 pip3 --version
-
 
 rm -f /tmp/get-pip.py
