c752689585628ad6d6be92bbb53aa6da0b897b6a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-30 09:39:05 -0700

    allbranding

--- install-otel-cli-on-linux.sh
+++ install-otel-cli-on-linux.sh
@@ -3,7 +3,7 @@
 # https://github.com/equinix-labs/otel-cli
 # https://github.com/equinix-labs/otel-cli/releases
 
-url=$(/usr/local/bin/allbranding query --releases-url=https://api.github.com/repos/equinix-labs/otel-cli/releases --asset-regex='otel-cli.*_linux_amd64.tar.gz' | jq -r .browser_download_url)
+url=$(/home/linuxbrew/.linuxbrew/bin/allbranding query --releases-url=https://api.github.com/repos/equinix-labs/otel-cli/releases --asset-regex='otel-cli.*_linux_amd64.tar.gz' | jq -r .browser_download_url)
 curl -fsSL $url | tar -tz
 curl -fsSL $url | tar --no-same-owner -C /usr/local/bin -xz otel-cli
 
