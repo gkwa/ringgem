77ad4de46505c9b44ac4180687cd0fe88cc6c8f8
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-30 16:01:44 -0700

    mythai

--- install-mythai-on-linux.sh
+++ install-mythai-on-linux.sh
@@ -2,7 +2,10 @@
 
 export PATH=/home/linuxbrew/.linuxbrew/bin:"$PATH"
 
-git clone https://github.com/taylormonacelli/mythai.git
+if [[ ! -d mythai ]]; then
+    git clone https://github.com/taylormonacelli/mythai.git
+fi
+
 cd mythai
 rye sync
 . .venv/bin/activate
