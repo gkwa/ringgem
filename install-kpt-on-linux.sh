8cb7a3973ab9a8b03b3b53be64504aab7cce2780
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-02 08:28:50 -0700

    kpt

---
+++ install-kpt-on-linux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# https://kpt.dev/installation/kpt-cli
+# https://github.com/kptdev/kpt/blob/main/Formula/kpt.rb
+
+sudo --login --user linuxbrew brew tap GoogleContainerTools/kpt https://github.com/GoogleContainerTools/kpt.git
+sudo --login --user linuxbrew brew install kpt
