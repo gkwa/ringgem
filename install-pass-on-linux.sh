665df4a45ee6070c14a91b019549a4cba21103c5
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-05 23:19:44 -0700

    Add pass

--- install-pass-on-linux.sh
+++ install-pass-on-linux.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
 
-# Check the host and perform the corresponding action
-case "$(hostname)" in
-  "ubuntu" | "debian")
-    sudo apt-get --assume-yes install pass
-    ;;
-  "fedora" | "rhel")
-    sudo yum install pass
-    ;;
-  "opensuse")
-    sudo zypper in password-store
-    ;;
-  "gentoo")
-    emerge -av pass
-    ;;
-  "arch")
-    pacman -S pass
-    ;;
-  "darwin")
-    brew install pass
-    ;;
-  "freebsd")
-    pkg install password-store
-    ;;
-  *)
-    echo "Unsupported host. Please install password-store manually."
-    ;;
-esac
+if command -v apt-get &> /dev/null; then
+   sudo apt-get --assume-yes install pass
+elif command -v yum &> /dev/null; then
+   sudo yum install pass
+elif command -v zypper &> /dev/null; then
+   sudo zypper in password-store
+elif command -v emerge &> /dev/null; then
+   emerge -av pass
+elif command -v pacman &> /dev/null; then
+   pacman -S pass
+elif command -v brew &> /dev/null; then
+   brew install pass
+elif command -v pkg &> /dev/null; then
+   pkg install password-store
+else
+   echo "Unsupported package manager. Please install password-store manually."
+fi
