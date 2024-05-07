65244b21f952a907a67233c85ec59874ce4f315a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-07 15:49:07 -0700

    mythai permission denied

--- install-mythai-on-linux.sh
+++ install-mythai-on-linux.sh
@@ -4,11 +4,11 @@ export PATH=/home/linuxbrew/.linuxbrew/bin:"$PATH"
 
 pwd
 
-[[ -x ./mythai/.venv/bin/activate ]] && exit 0
+[[ -f ./mythai/.venv/bin/activate ]] && exit 0
 
 git clone https://github.com/taylormonacelli/mythai.git
 
 rye sync --pyproject=./mythai/pyproject.toml
 
-./mythai/.venv/bin/activate
+. ./mythai/.venv/bin/activate
 mythai --help
