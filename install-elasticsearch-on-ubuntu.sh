e5626742a2dfb70952a45daa073420bd7b80f604
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-20 15:47:25 -0700

    DPkg::Lock::Timeout added in packer shell provisioner should prevent having to apt-get with retries

--- install-elasticsearch-on-ubuntu.sh
+++ install-elasticsearch-on-ubuntu.sh
@@ -6,9 +6,4 @@ set -u
 
 export DEBIAN_FRONTEND=noninteractive
 
-timeout 10m bash -c "
-    until apt-get install --fix-missing --assume-yes elasticsearch
-    do
-        sleep 10s
-    done
-"
+apt-get install --fix-missing --assume-yes elasticsearch
