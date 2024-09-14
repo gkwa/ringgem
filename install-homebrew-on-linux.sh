58f750136c1e4b8f90de6d80dc4505a39ecbbf83
Taylor Monacelli <taylormonacelli@gmail.com>
2024-09-14 09:39:43 -0700

    Fix this: bash: line 1: brew: command not found

--- install-homebrew-on-linux.sh
+++ install-homebrew-on-linux.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 
 if command -v apt-get &>/dev/null || command -v apt &>/dev/null; then
@@ -26,49 +25,40 @@ fi
 
 script=$(mktemp /tmp/homebrew-XXXXX.sh)
 chmod a+rx $script
-
 cat >$script <<'EOF'
 # Wait for dns
 timeout 30s curl --retry 9999 --connect-timeout 1 -sSf https://www.google.com >/dev/null
-
 export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH # suppress warning
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
 source ~/.bashrc
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/linuxbrew/.profile
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
 if ! grep HOMEBREW_AUTO_UPDATE_SECS ~/.profile; then
   cat >> ~/.profile <<EOF1
 export HOMEBREW_AUTO_UPDATE_SECS=$((24*60*60)) # 1 day
 EOF1
 fi
-
 brew --version
 EOF
 
 cd /home/linuxbrew
-
 if sudo -u linuxbrew brew --version >/dev/null; then
     # its already there
     exit 0
 fi
 
 sudo --login --user linuxbrew bash -e $script
-
 test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >>~/.bashrc
-
 rm -f $script
 
-# Update /etc/skel/.profile and /etc/skel/.bashrc
-update_skel_file() {
+# Function to update initialization files
+update_init_file() {
     local file="$1"
     local backup="${file}.bak"
-
     # Backup the original file
-    sudo cp "$file" "$backup"
-
+    cp "$file" "$backup"
     # Append Homebrew setup to the file
     echo '
 # Setup Homebrew if it exists
@@ -77,9 +67,14 @@ if [ -d "/home/linuxbrew/.linuxbrew" ]; then
 else
     echo "Homebrew is not installed in the expected location." >&2
 fi
-' | sudo tee -a "$file" > /dev/null
-
+' | tee -a "$file" > /dev/null
     echo "Updated $file"
+}
+
+# Update /etc/skel/.profile and /etc/skel/.bashrc
+update_skel_file() {
+    local file="$1"
+    sudo update_init_file "$file"
 }
 
 # Update /etc/skel/.profile
@@ -88,4 +83,8 @@ update_skel_file "/etc/skel/.profile"
 # Update /etc/skel/.bashrc
 update_skel_file "/etc/skel/.bashrc"
 
-echo "Homebrew installation and skel file updates completed successfully."
+# Update root's initialization files
+update_init_file "/root/.profile"
+update_init_file "/root/.bashrc"
+
+echo "Homebrew installation, skel file updates, and root initialization file updates completed successfully."
