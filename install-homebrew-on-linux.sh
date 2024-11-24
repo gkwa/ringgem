07c66df9963bc8aa3fae72f8ab59eb95c028fb74
Taylor Monacelli <taylormonacelli@gmail.com>
2024-11-23 22:15:30 -0800

    homebrew

--- install-homebrew-on-linux.sh
+++ install-homebrew-on-linux.sh
@@ -4,100 +4,81 @@ set -e
 HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
 HOMEBREW_SHELLENV_COMMAND='eval "$('${HOMEBREW_PREFIX}'/bin/brew shellenv)"'
 
-# Function to install required packages
 install_packages() {
-    if command -v apt-get &>/dev/null || command -v apt &>/dev/null; then
-        apt-get update
-        sudo apt-get install --assume-yes build-essential procps curl file git
-    elif [ -f /etc/fedora-release ] || [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
-        sudo yum -y groupinstall 'Development Tools'
-        sudo yum -y install procps-ng curl file git
-    elif [ -f /etc/arch-release ]; then
-        sudo pacman -S base-devel procps-ng curl file git
-    else
-        echo "Unsupported distribution"
-        exit 1
-    fi
+   if command -v apt-get &>/dev/null || command -v apt &>/dev/null; then
+       apt-get update
+       sudo apt-get install --assume-yes build-essential procps curl file git
+   elif [ -f /etc/fedora-release ] || [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
+       sudo yum -y groupinstall 'Development Tools'
+       sudo yum -y install procps-ng curl file git
+   elif [ -f /etc/arch-release ]; then
+       sudo pacman -S base-devel procps-ng curl file git
+   else
+       echo "Unsupported distribution"
+       exit 1
+   fi
 }
 
-# Function to setup linuxbrew user
 setup_linuxbrew_user() {
-    if ! id -u linuxbrew &>/dev/null; then
-        sudo useradd --create-home linuxbrew --shell /bin/bash
-    fi
-    if ! test -f /etc/sudoers.d/linuxbrew; then
-        echo "linuxbrew ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/linuxbrew
-    fi
+   if ! id -u linuxbrew &>/dev/null; then
+       sudo useradd --create-home linuxbrew --shell /bin/bash
+   fi
+   if ! test -f /etc/sudoers.d/linuxbrew; then
+       echo "linuxbrew ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/linuxbrew
+   fi
 }
 
-# Function to add Homebrew shellenv to a file
 add_homebrew_shellenv() {
-    local file="$1"
-    if ! grep -q "Setup Homebrew if it exists" "$file"; then
-        echo "
-# Setup Homebrew if it exists
-if [ -d \"$HOMEBREW_PREFIX\" ]; then
-    $HOMEBREW_SHELLENV_COMMAND
-else
-    echo \"Homebrew is not installed in the expected location.\" >&2
-fi" >> "$file"
-        echo "Updated $file with Homebrew shellenv"
-    else
-        echo "Homebrew shellenv already exists in $file, skipping."
-    fi
+   local file="$1"
+   if ! grep -q "export PATH=\"/usr/local/bin:\$PATH\"" "$file"; then
+       echo 'export PATH="/usr/local/bin:$PATH"' >> "$file"
+       echo "Updated $file with PATH priority"
+   fi
 }
 
-# Function to create and execute Homebrew installation script
 install_homebrew() {
-    local script=$(mktemp /tmp/homebrew-XXXXX.sh)
-    chmod a+rx $script
-    cat >$script <<EOF
-# Wait for dns
+   local script=$(mktemp /tmp/homebrew-XXXXX.sh)
+   chmod a+rx $script
+   cat >$script <<EOF
 timeout 30s curl --retry 9999 --connect-timeout 1 -sSf https://www.google.com >/dev/null
-export PATH=${HOMEBREW_PREFIX}/bin:\$PATH # suppress warning
+export PATH=${HOMEBREW_PREFIX}/bin:\$PATH
 NONINTERACTIVE=1 /bin/bash -c "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 $HOMEBREW_SHELLENV_COMMAND
 if ! grep HOMEBREW_AUTO_UPDATE_SECS ~/.profile; then
-  echo 'export HOMEBREW_AUTO_UPDATE_SECS=\$((24*60*60)) # 1 day' >> ~/.profile
+ echo 'export HOMEBREW_AUTO_UPDATE_SECS=\$((24*60*60))' >> ~/.profile
 fi
 brew --version
 EOF
-    cd /home/linuxbrew
-    if [ ! -f "${HOMEBREW_PREFIX}/bin/brew" ]; then
-        sudo --login --user linuxbrew bash -e $script
-    else
-        echo "Homebrew is already installed. Skipping installation."
-    fi
-    rm -f $script
+   cd /home/linuxbrew
+   if [ ! -f "${HOMEBREW_PREFIX}/bin/brew" ]; then
+       sudo --login --user linuxbrew bash -e $script
+   else
+       echo "Homebrew is already installed. Skipping installation."
+   fi
+   rm -f $script
 }
 
-# Function to create and install the brew wrapper script
 install_brew_wrapper() {
-    cat > /usr/local/bin/brew <<EOF
+   cat > /usr/local/bin/brew <<'EOF'
 #!/bin/bash
-
-# Run brew command as linuxbrew user
-sudo --user linuxbrew --login brew "\$@"
+exec /usr/local/bin/brew "$@"
 EOF
 
-    chmod +x /usr/local/bin/brew
-    echo "Installed brew wrapper script to /usr/local/bin/brew"
+   chmod +x /usr/local/bin/brew
+   echo "Installed brew wrapper script to /usr/local/bin/brew"
 }
 
-# Main execution
 install_packages
 setup_linuxbrew_user
 install_homebrew
 
-# Update shell environment
 if [ -f "${HOMEBREW_PREFIX}/bin/brew" ]; then
-    eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
+   eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
 else
-    echo "Homebrew installation seems to have failed. Please check the logs."
-    exit 1
+   echo "Homebrew installation seems to have failed. Please check the logs."
+   exit 1
 fi
 
-# Update initialization files
 add_homebrew_shellenv ~/.bashrc
 add_homebrew_shellenv ~/.profile
 add_homebrew_shellenv "/etc/skel/.profile"
@@ -107,7 +88,6 @@ add_homebrew_shellenv "/root/.bashrc"
 add_homebrew_shellenv "/home/linuxbrew/.bashrc"
 add_homebrew_shellenv "/home/linuxbrew/.profile"
 
-# Install brew wrapper script
 install_brew_wrapper
 
-echo "Homebrew installation, skel file updates, root initialization file updates, and brew wrapper script installation completed successfully."
+echo "Homebrew installation completed successfully."
