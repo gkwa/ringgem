98f3f028330a94f30f98b30f7abba212fb3e44f5
Taylor Monacelli <taylormonacelli@gmail.com>
2024-12-18 22:47:02 -0500

    improve homebrew install

--- install-homebrew-on-linux.sh
+++ install-homebrew-on-linux.sh
@@ -2,4 +2,3 @@
 set -e
 
 curl -fsSL https://raw.githubusercontent.com/gkwa/alittlepaintball/4c38463e37dd9a3fffa335b6680ffb28bd930a18/001-homebrew-base.sh | bash
-curl -fsSL https://raw.githubusercontent.com/gkwa/alittlepaintball/4c38463e37dd9a3fffa335b6680ffb28bd930a18/002-homebrew-configured.sh | bash
