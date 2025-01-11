cc398a4f9603b62235c413bef6bda17715b38775
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-10 22:28:55 -0800

    test

--- install-python-on-linux.sh
+++ install-python-on-linux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-[ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
+# [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
 if [ -f /etc/fedora-release ]; then
     dnf install -y python3
