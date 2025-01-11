0c57441307f1d81bf041344475ef266353770e46
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-10 22:11:14 -0800

    workaround https://github.com/gkwa/grayfiction/actions/runs/12719234260/job/35459088663#step:7:1784

--- install-python-pip-on-linux.sh
+++ install-python-pip-on-linux.sh
@@ -2,14 +2,10 @@
 
 [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-python3 /tmp/get-pip.py
-
+brew install python
 python3 --version
 pip3 --version
 
 type -a pip3
 
 realpath /usr/local/bin/pip3
-
-rm -f /tmp/get-pip.py
