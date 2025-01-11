a34a67c211b7a3fd6ad28cf51cf2ebbad7669486
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-10 22:32:08 -0800

    test

--- install-python-pip-on-linux.sh
+++ install-python-pip-on-linux.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-[ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
+# [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-brew install python
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python3 /tmp/get-pip.py
+
 python3 --version
 pip3 --version
 
 type -a pip3
 
 realpath /usr/local/bin/pip3
+
+rm -f /tmp/get-pip.py
