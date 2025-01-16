6396ba375ef1d946da92288dc863f8126a982982
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-16 12:04:46 -0800

    debug

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-pip3 install ansible --user
+echo "Debugging information:"
+echo "---------------------"
+
+echo "PATH:"
+echo "$PATH"
+
+echo "Which pip3:"
+which pip3
+
+echo "Type pip3:"
+type -a pip3
+
+echo "---------------------"
+
+# pip3 install ansible --user
+brew install ansible
