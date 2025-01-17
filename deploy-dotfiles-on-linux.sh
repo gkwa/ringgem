70d1a8411f65269ef585975168f2b7bb56192043
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-16 16:42:21 -0800

    chezmoi don't try to get tty when deploying dotfiles

--- deploy-dotfiles-on-linux.sh
+++ deploy-dotfiles-on-linux.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-chezmoi init --depth 1 --apply gkwa/dotfiles
+chezmoi init --no-tty --force --depth 1 --apply gkwa/dotfiles
