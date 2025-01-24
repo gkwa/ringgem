00010bf99a30d237c3f6a134a87f2782db53ab97
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-23 20:16:20 -0800

    hydrate

--- deploy-dotfiles-on-linux.sh
+++ deploy-dotfiles-on-linux.sh
@@ -2,3 +2,6 @@
 [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
 chezmoi init --no-tty --force --depth 1 --apply gkwa/dotfiles
+
+# hydrate the dotfiles
+time bash -l -c 'echo hello'
