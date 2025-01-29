3c5fa79558f5fb2908d2e275bb46bc2fe40037d9
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-29 15:43:10 -0800

    deploy full dotfiles instead of depth 1

--- deploy-dotfiles-on-linux.sh
+++ deploy-dotfiles-on-linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-chezmoi init --no-tty --force --depth 1 --apply gkwa/dotfiles
+chezmoi init --no-tty --force --apply gkwa/dotfiles
 
 # hydrate the dotfiles
 time bash -l -c 'echo hello'
