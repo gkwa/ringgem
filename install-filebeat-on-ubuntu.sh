423b693db1036d41730220b829de9f39574051b1
Taylor Monacelli <taylormonacelli@gmail.com>
2025-04-17 09:05:51 -0700

    fix filebeat install

--- install-filebeat-on-ubuntu.sh
+++ install-filebeat-on-ubuntu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-curl -o /tmp/filebeat-installation-configuration.html https://www.elastic.co/guide/en/beats/filebeat/current/filebeat-installation-configuration.html
+curl -o /tmp/filebeat-installation-configuration.html https://www.elastic.co/downloads/beats/filebeat
 version=$(cat /tmp/filebeat-installation-configuration.html | grep -oP 'filebeat-\K[\d.]+(?=-amd64\.deb)' | head -n 1)
 echo "Latest Filebeat version: $version"
 curl -L -o /tmp/filebeat-${version}-amd64.deb https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-${version}-amd64.deb
