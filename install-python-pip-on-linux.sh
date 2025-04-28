a6552ef045b72eb89695a6e09b143d03de2bc36c
Taylor Monacelli <taylormonacelli@gmail.com>
2025-04-28 08:20:28 -0700

    fix pip install

--- install-python-pip-on-linux.sh
+++ install-python-pip-on-linux.sh
@@ -2,14 +2,26 @@
 
 # [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+# Default pip URL
+PIP_URL="https://bootstrap.pypa.io/get-pip.py"
+
+# Get Python version
+PYTHON_VERSION=$(python3 -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")
+
+# Override URL for Python 3.8
+if [[ $PYTHON_VERSION == "3.8" ]]; then
+    PIP_URL="https://bootstrap.pypa.io/pip/3.8/get-pip.py"
+fi
+
+# Download and run pip installer
+curl $PIP_URL -o /tmp/get-pip.py
 python3 /tmp/get-pip.py
 
+# Show version info
 python3 --version
 pip3 --version
 
 type -a pip3
-
 realpath /usr/local/bin/pip3
 
 rm -f /tmp/get-pip.py
