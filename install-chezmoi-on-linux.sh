d012d8b731a6c9b7cf5959de0a96a10ee12a74d0
Taylor Monacelli <taylormonacelli@gmail.com>
2025-07-02 17:21:37 -0400

    dotfiles

---
+++ install-chezmoi-on-linux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+[ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
+
+sudo --login --user linuxbrew bash -c '
+  [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh;
+  brew install chezmoi
+'
