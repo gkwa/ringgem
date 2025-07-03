ba7d996f2305b3d0c6f64023970558c2ce134193
Taylor Monacelli <taylormonacelli@gmail.com>
2025-07-02 21:29:17 -0400

    golangci-lint

--- install-golangci-lint-on-linux-using-go.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-[ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
-
-export PATH=$PATH:$(/usr/local/go/bin/go env GOPATH)/bin
-
-/usr/local/go/bin/go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
-
-golangci-lint --version
