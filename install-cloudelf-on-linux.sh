075f821ec1c39c726a19177dcab1e5a3fe30d998
Taylor Monacelli <taylormonacelli@gmail.com>
2025-07-02 20:24:34 -0400

    cloudelf

--- install-cloudelf-on-linux.sh
+++ install-cloudelf-on-linux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-sudo --login --user linuxbrew bash -c '
-  [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh;
-  brew install gkwa/tools/cloudelf
-'
+brew install gkwa/tools/cloudelf
+
+cloudelf -h
+
