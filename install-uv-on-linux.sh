0d0eeacf2d48105c32c0d1e453757c6b101c758a
Taylor Monacelli <taylormonacelli@gmail.com>
2025-07-10 10:29:45 -0700

    uv

--- install-uv-on-linux.sh
+++ install-uv-on-linux.sh
@@ -5,3 +5,11 @@ brew install uv
 
 export PATH="$HOME/.local/bin:$PATH"
 uv tool update-shell
+
+# We need this:
+# # export PATH="$HOME/.local/bin:$PATH"
+# because of this:
+# [root@ip-172-31-23-184:~]$ uv tool update-shell --quiet; echo $?
+# error: The executable directory /root/.local/bin is not in PATH, but the Bash configuration files are already up-to-date
+# 2
+# [root@ip-172-31-23-184:~]$
