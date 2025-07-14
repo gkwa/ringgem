02f01df5d16c417748cb0011f7d944057dc78a93
Taylor Monacelli <taylormonacelli@gmail.com>
2025-07-13 19:16:32 -0700

    mythai

--- install-mythai-on-linux.sh
+++ install-mythai-on-linux.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 [ -s /etc/profile.d/homebrew.sh ] && source /etc/profile.d/homebrew.sh
 
-[[ -f /opt/mythai/.venv/bin/activate ]] && exit 0
+uv tool install https://github.com/gkwa/mythai/archive/refs/heads/master.zip
 
-git clone https://github.com/taylormonacelli/mythai.git /opt/mythai
-
-uv sync --project=/opt/mythai/pyproject.toml
-
-. /opt/mythai/.venv/bin/activate
 mythai --help
